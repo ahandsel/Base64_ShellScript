e889141bcf624f39fc816e2e3ecdde7fb02c1971
Genji Fujimori-Smith <30670749+ahandsel@users.noreply.github.com>
2019-10-23 13:20:34 +0900

    Bug Fix: Replacing "-" with "_"
    Bug Fix to address filenames coding "-" to have "_" in their javascript variable name

--- base64_JS_Variable.sh
+++ base64_JS_Variable.sh
@@ -19,11 +19,13 @@ echo "$# number of files has been detected."
 
 for file in "$@"
 do
-  echo "let ${file%.*}_base64 = '"$(echo "data:")$(file --mime-type -b "$file")";base64,"$(base64 -i "$file")"';" >> Base64_Output.js
-  echo "" >> Base64_Output.js
+  TEMP="${file/%.*}"
+  echo "let ${TEMP//-/_}_base64 = '"$(echo "data:")$(file --mime-type -b "$file")";base64,"$(base64 -i "$file")"';" >> Base64_Output_TEST.js
+  # echo "let ${file%.*}_base64 = '"$(echo "data:")$(file --mime-type -b "$file")";base64,"$(base64 -i "$file")"';" >> Base64_Output.js
+  echo "" >> Base64_Output_TEST.js
 done
-echo "The encoding is done. The base64 are stored as variables in the file \"Base64_Output.js\"."
-
+echo "The encoding is done."
+echo "The base64 strings are stored as variables in the \"Base64_Output.js\" file."
 
 # Shout Out to @kylewlacy for his Base64.sh that I used as a reference.
 # https://gist.github.com/kylewlacy/2820377
